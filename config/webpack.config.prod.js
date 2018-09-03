a803584f7c5c5ac2b6b30a983b01380c4a0858e8
WaneLiu <825060952@qq.com>
2018-09-04 00:04:55 +0800

    nothing

--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -339,4 +339,26 @@ module.exports = {
     tls: 'empty',
     child_process: 'empty',
   },
+  optimization: {
+    splitChunks: {
+      chunks: 'async',
+      minSize: 30000,
+      minChunks: 1,
+      maxAsyncRequests: 5,
+      maxInitialRequests: 3,
+      automaticNameDelimiter: '~',
+      name: true,
+      cacheGroups: {
+        vendors: {
+          test: /[\\/]node_modules[\\/]/,
+          priority: -10
+        },
+        default: {
+          minChunks: 2,
+          priority: -20,
+          reuseExistingChunk: true
+        }
+      }
+    }
+  },
 };
--- src/components/searchPage.js
+++ src/components/searchPage.js
@@ -1,6 +1,6 @@
 import React, { PureComponent } from 'react'
 //import PropTypes from 'prop-types'
-import history from '../router/history'
+//import history from '../router/history'
 import api from '../modules/api/api'
 import { BackTop } from 'antd';
 import BookList from './common/common-modules/BookList'
